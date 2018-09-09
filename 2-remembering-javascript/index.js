8b4d0027e6336dffb161aec26743b4d7cb64a480
pilyichev <37084423+pilyichev@users.noreply.github.com>
2018-09-10 00:14:54 +0300

    Delete index.js

--- 2-remembering-javascript/index.js
+++
@@ -1,9 +0,0 @@
-let numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
-
-let totally = 0;
-for (let i = 0; i < numbers.length; i++);
-console.log(`Сумма: ` + totally);
-
-let average = totally / numbers.legth;
-console.log(`Среднее арифметическое: ` + average);
