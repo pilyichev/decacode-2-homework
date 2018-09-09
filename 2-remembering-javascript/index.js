5c1a66623f1b2fb6b2be17926f08d4f80b71c341
ILICHEV PAVEL (MOSCOW) <PILICH25@neptune.dkcorp.net>
2018-09-09 23:55:19 +0300

    Add first homework

---
+++ 2-remembering-javascript/index.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+let totally = 0;
+for (let i = 0; i < numbers.length; i++);
+console.log(`Сумма: ` + totally);
+
+let average = totally / numbers.legth;
+console.log(`Среднее арифметическое: ` + average);
